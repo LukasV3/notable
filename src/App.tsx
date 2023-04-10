79dcd4346ecba563989186eb988203fb2bb3b452
LukasV3 <volk.lukas237@yahoo.co.uk>
2023-04-10 13:56:47 +0200

    Created note layout wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useLocalStorage } from "./UseLocaleStorage";
 import { useMemo } from "react";
 import { v4 as uuidV4 } from "uuid";
 import { NoteList } from "./NoteList";
+import { NoteLayout } from "./NoteLayout";
 
 export type Note = {
   id: string;
@@ -63,7 +64,7 @@ function App() {
             <NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />
           }
         />
-        <Route path="/:id">
+        <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
         </Route>
