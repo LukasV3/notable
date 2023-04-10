31ce6d24d69a57a391250cbb051675e59614ca19
LukasV3 <volk.lukas237@yahoo.co.uk>
2023-04-10 12:09:49 +0200

    Added tag creation logic

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,16 +1,20 @@
 import { FormEvent, useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
 import { NoteData, Tag } from "./App";
+import { v4 as uuidV4 } from "uuid";
 
 type NoteFormProps = {
   onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
 };
 
-export function NoteForm({ onSubmit }: NoteFormProps) {
+export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const navigate = useNavigate();
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
@@ -18,8 +22,10 @@ export function NoteForm({ onSubmit }: NoteFormProps) {
     onSubmit({
       title: titleRef.current!.value,
       markdown: markdownRef.current!.value,
-      tags: [],
+      tags: selectedTags,
     });
+
+    navigate("..");
   }
 
   return (
@@ -38,7 +44,15 @@ export function NoteForm({ onSubmit }: NoteFormProps) {
         <div className="flex flex-col gap-1">
           <label htmlFor="react-select-3-input">Tags</label>
           <CreatableReactSelect
+            onCreateOption={(label) => {
+              const newTag = { id: uuidV4(), label };
+              onAddTag(newTag);
+              setSelectedTags((prev) => [...prev, newTag]);
+            }}
             value={selectedTags.map((tag) => ({ label: tag.label, value: tag.id }))}
+            options={availableTags.map((tag) => {
+              return { label: tag.label, value: tag.id };
+            })}
             onChange={(tags) =>
               setSelectedTags(tags.map((tag) => ({ label: tag.label, id: tag.value })))
             }
