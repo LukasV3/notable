6a8e1fe12b0759fc9b6a96941e662956e0c73762
LukasV3 <volk.lukas237@yahoo.co.uk>
2023-04-11 08:46:04 +0200

    Created edit and delete tags modal

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
 import { Tag } from "./App";
 import { useMemo, useState } from "react";
+import { createPortal } from "react-dom";
 
 type SimplifiedNote = {
   tags: Tag[];
@@ -12,11 +13,26 @@ type SimplifiedNote = {
 type NoteListProp = {
   availableTags: Tag[];
   notes: SimplifiedNote[];
+  onUpdateTag: (id: string, label: string) => void;
+  onDeleteTag: (id: string) => void;
 };
 
-export function NoteList({ availableTags, notes }: NoteListProp) {
+type EditTagsModalProps = {
+  onClose: () => void;
+  availableTags: Tag[];
+  onUpdateTag: (id: string, label: string) => void;
+  onDeleteTag: (id: string) => void;
+};
+
+export function NoteList({
+  availableTags,
+  notes,
+  onDeleteTag,
+  onUpdateTag,
+}: NoteListProp) {
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
   const [title, setTitle] = useState("");
+  const [showEditTagsModal, setShowEditTagsModal] = useState(false);
 
   const filteredNotes = useMemo(() => {
     return notes.filter((note) => {
@@ -38,7 +54,12 @@ export function NoteList({ availableTags, notes }: NoteListProp) {
             <button className="border border-grey rounded px-2.5 py-1">Create</button>
           </Link>
 
-          <button className="border border-grey rounded px-2.5 py-1">Edit Tags</button>
+          <button
+            onClick={() => setShowEditTagsModal(true)}
+            className="border border-grey rounded px-2.5 py-1"
+          >
+            Edit Tags
+          </button>
         </div>
       </div>
 
@@ -76,6 +97,17 @@ export function NoteList({ availableTags, notes }: NoteListProp) {
           <NoteCard key={note.id} id={note.id} title={note.title} tags={note.tags} />
         ))}
       </div>
+
+      {showEditTagsModal &&
+        createPortal(
+          <EditTagsModal
+            onClose={() => setShowEditTagsModal(false)}
+            availableTags={availableTags}
+            onUpdateTag={onUpdateTag}
+            onDeleteTag={onDeleteTag}
+          />,
+          document.body
+        )}
     </>
   );
 }
@@ -101,5 +133,49 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
         </ul>
       )}
     </Link>
+  );
+}
+
+function EditTagsModal({
+  onClose,
+  availableTags,
+  onUpdateTag,
+  onDeleteTag,
+}: EditTagsModalProps) {
+  return (
+    <div className="fixed inset-0 z-50 h-full w-full bg-gray-600/60 flex justify-center items-start">
+      <div className="container bg-white rounded p-5 mt-10 mx-10 w-full flex flex-col gap-y-6">
+        <div className="flex items-center justify-between">
+          <h2>Edit Tags</h2>
+
+          <button onClick={onClose} className="text-3xl">
+            &times;
+          </button>
+        </div>
+
+        {availableTags.length > 0 && (
+          <ul className="flex flex-col gap-y-2">
+            {availableTags.map((tag) => {
+              return (
+                <li key={tag.id} className="flex gap-x-3">
+                  <input
+                    onChange={(e) => onUpdateTag(tag.id, e.target.value)}
+                    defaultValue={tag.label}
+                    className="grow border border-grey rounded px-2.5 py-1"
+                  />
+
+                  <button
+                    onClick={() => onDeleteTag(tag.id)}
+                    className="border border-grey rounded px-2.5 py-1"
+                  >
+                    &times;
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
+    </div>
   );
 }
