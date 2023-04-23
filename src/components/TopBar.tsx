0f215792ba3c386b4e1f875e8da5c91a3b2d52fd
LukasV3 <volk.lukas237@yahoo.co.uk>
2023-04-23 12:42:10 +0200

    Added dark mode toggle logic to TopBar component

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
+
 export function TopBar() {
+  const [isDarkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    const htmlEl = document.querySelector("html");
+    isDarkMode ? htmlEl?.classList.add("dark") : htmlEl?.classList.remove("dark");
+  }, [isDarkMode]);
+
   return (
     <div className="flex justify-between items-center border-b border-grey-border mb-5 pb-4 dark:border-grey">
       <h1 className="text-3xl font-black text-transparent bg-clip-text bg-gradient-to-r from-volt to-grey">
         Notable
       </h1>
-
-      <div>Light mode</div>
+      <DarkModeSwitch checked={isDarkMode} onChange={(checked) => setDarkMode(checked)} />
     </div>
   );
 }
