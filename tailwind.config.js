ea648dc74eea9d4435a2090fbf9ad309d76b5d73
Lukas Volk <volk.lukas237@yahoo.co.uk>
2024-12-24 16:05:50 +0000

    Initialise shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,74 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        grey: { 
-          DEFAULT: '#2b2c2f',
-          dark: '#1b1e21',
-          border: '#edeee7',
-          "border-dark": '#1c1d1f'
+        grey: {
+          DEFAULT: "#2b2c2f",
+          dark: "#1b1e21",
+          border: "#edeee7",
+          "border-dark": "#1c1d1f",
         },
         volt: {
-          DEFAULT: '#caf558',
-          light: '#f9faf5'
-        }
-      }
+          DEFAULT: "#caf558",
+          light: "#f9faf5",
+        },
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
     fontFamily: {
-      'sans': ['Poppins', 'sans-serif']
+      sans: ["Poppins", "sans-serif"],
     },
     container: {
       center: true,
-      padding: '1rem',
+      padding: "1rem",
     },
   },
-  darkMode: 'class',
-  plugins: [],
-}
+  darkMode: ["class"],
+  plugins: [require("tailwindcss-animate")],
+};
